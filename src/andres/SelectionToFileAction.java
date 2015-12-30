21881c81a9e835d92d32ecd8e9fad2ac0387b2e2
Andres Dominguez <andres.dominguez.p@gmail.com>
2015-12-30 17:37:19 -0500

    initial commit

---
+++ src/andres/SelectionToFileAction.java
@@ -0,0 +1,47 @@
+package andres;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
+
+public class SelectionToFileAction extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    Editor editor = e.getData(PlatformDataKeys.EDITOR);
+    Project project = e.getProject();
+
+    if (editor == null || project == null) {
+      return;
+    }
+
+    String selectedText = editor.getSelectionModel().getSelectedText();
+    if (selectedText == null) {
+      return;
+    }
+
+    PsiFile file = e.getData(LangDataKeys.PSI_FILE);
+    if (file == null) {
+      return;
+    }
+
+    String fileName = "ando";
+    FileType fileType = file.getFileType();
+
+    ApplicationManager.getApplication().runWriteAction(new Runnable() {
+      @Override
+      public void run() {
+        PsiFile newFile = PsiFileFactory.getInstance(project)
+            .createFileFromText(fileName, fileType, selectedText);
+        file.getContainingDirectory().add(newFile);
+      }
+    });
+  }
+}
